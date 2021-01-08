fb27e8180dd69a27b70e5e758315f598e0a0d3e1
Simon Geiger <simongeiger01@gmail.com>
2021-01-09 03:08:50 +0900

    adjusted code for jupyter notebook

--- src/Image.py
+++ src/Image.py
@@ -1,70 +1,7 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-class Line():
-  """
-  This Line class stores the parameters (start and end points, slope m and y-interception b) of every line.
-  Additionally it evaluates (depending on slope value) if the line is potentially part of the left or right lane
-  """
-  def __init__(self, *args):
-    '''
-    This constructor initializes the line object. Two cases are accepted as input [start + end point + image_shape]
-    or [slope + y-interception + classication + image_shape]. Lines are drawn depending on image size.
-    '''
-    ### constructor for start and end point input
-    if len(args) == 5:
-      self.start_x = args[0]
-      self.start_y = args[1]
-      self.end_x = args[2]
-      self.end_y = args[3]
-      self.image_shape = args[4]
-
-      if self.end_x != self.start_x:
-        self.m = (self.end_y - self.start_y) / (self.end_x - self.start_x)
-        self.b = self.start_y - self.m * self.start_x 
-      else:
-        self.m = np.Infinity
-        self.b = np.Infinity
-
-      # self.length = np.sqrt( (self.end_x - self.start_x)**2 + (self.end_y - self.start_y)**2)
-
-      ### orientation classification depending on value of slope m
-      m_wanted_left = 0.55
-      m_wanted_right = -0.68
-      m_tolerance = 0.25
-      if self.m > (m_wanted_left - m_tolerance) and  self.m < (m_wanted_left + m_tolerance):
-          # left line => / 
-          self.type = 'left'
-      elif self.m > (m_wanted_right - m_tolerance) and self.m < (m_wanted_right + m_tolerance):
-          # right line => \ 
-          self.type = 'right'
-      else:
-          # line to the left or to the right => not useful
-          self.type = 'horizontal'
-
-    ### constructor for slope, y-interception and classifiation as input
-    elif len(args) == 4:
-      self.m = args[0]
-      self.b = args[1]
-      self.type = args[2]
-      self.image_shape = args[3]
-      self.start_y = np.int32(np.rint(self.image_shape[1]/960 * 540)) # lower edge of image
-      self.start_x = np.int32(np.rint((self.start_y - self.b) / self.m))
-      self.end_y = np.int32(np.rint(self.image_shape[1]/960 * 330)) # draw line up to this height
-      self.end_x = np.int32(np.rint((self.end_y - self.b) / self.m))
-
-    ### error, because wrong number of inputs
-    else:
-      raise NotImplementedError(f"Constructor for {len(args)} has not been defined")
-
-  def draw_line(self, img, color = [255, 0, 255], thickness = 2):
-    '''
-    This function draws a line into a given image. Color and thickness can be parametrizied.
-    '''
-    cv2.line(img, (self.start_x, self.start_y), (self.end_x, self.end_y), color, thickness)  
-
+from src.Line import Line as Line
 
 class Image:
   """
@@ -97,18 +34,23 @@ class Image:
     - detection of road boundaries => better ROI?
   """
 
+  def __init__(self):
+    self.print_mode = 4
+
   def process_image(self, img, print_mode=0):
     """
     This function processes a single image and returns an image with detected lane lines highlighted.
     print_mode is an opptional value, with it steps of the algorithm can be visualized.
     """
+    self.print_mode = print_mode
+
     self.readImage(img)
     self.create_color_mask()
     self.find_hough_lines_in_roi()
     self.get_lanes()
 
-    if print_mode > 0:
-      self.print(print_mode)
+    if self.print_mode > 0:
+      self.print(self.print_mode)
 
     return cv2.cvtColor(self.lane_image,cv2.COLOR_RGB2BGR)
 
@@ -118,8 +60,8 @@ class Image:
       self.image_raw = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
       self.shape = self.image_raw.shape
     else:
-      # introduction to python exceptions => https://realpython.com/python-exceptions/
-      # how/when to use assert, raise, try/except => https://stackoverflow.com/questions/40182944/difference-between-raise-try-and-assert
+    #   # introduction to python exceptions => https://realpython.com/python-exceptions/
+    #   # how/when to use assert, raise, try/except => https://stackoverflow.com/questions/40182944/difference-between-raise-try-and-assert
       raise ValueError(f"Image is missing.")
 
   def create_color_mask(self):
@@ -133,15 +75,19 @@ class Image:
     # self.mask_white_color = cv2.GaussianBlur(self.mask_white_color, (kernel_size, kernel_size), 0)
 
     ### create color mask by filtering for yellow and white 
-    # lower_yellow = np.array([17, 60, 0], dtype="uint8")
-    # upper_yellow = np.array([25, 220, 255], dtype="uint8")
-    lower_yellow = np.array([75, 64, 0], dtype="uint8")
-    upper_yellow = np.array([105, 255, 255], dtype="uint8")
+    if self.print_mode != 0:
+      # range for test images in .png
+      lower_yellow = np.array([17, 60, 0], dtype="uint8")
+      upper_yellow = np.array([25, 220, 255], dtype="uint8")
+    else:
+      # range for clips in .mp4
+      lower_yellow = np.array([75, 64, 0], dtype="uint8")
+      upper_yellow = np.array([105, 255, 255], dtype="uint8")
+
     self.mask_yellow_color = cv2.inRange(self.image_hsv, lower_yellow, upper_yellow)
     lower_white = np.array([0,0,130], dtype="uint8")
     upper_white = np.array([255,60,255], dtype="uint8")
     self.mask_white_color = cv2.inRange(self.image_hsv, lower_white, upper_white)
-
   
     ### combine color masks and then mask image
     self.color_mask = cv2.bitwise_or(self.mask_white_color,self.mask_yellow_color)
@@ -173,7 +119,7 @@ class Image:
     for i, element in enumerate(self.hough_lines):
       self.lines.append(Line(element[0][0],element[0][1],element[0][2],element[0][3], self.shape))
       # self.lines[i].draw_line(self.hough_image, color=[0,255,0], thickness = 1)
-      # self.lines[i].draw_line(self.canny_hough_image, thickness = 1)
+      self.lines[i].draw_line(self.canny_hough_image, thickness = 1)
 
   def get_lanes(self):
     ### get average lane parameters (slope/intersection) for left and right
@@ -204,8 +150,8 @@ class Image:
     plt.rcParams.update({'font.size': 10})
 
     # plot figure in max window size
-    manager = plt.get_current_fig_manager()
-    manager.window.showMaximized()
+    # manager = plt.get_current_fig_manager()
+    # manager.window.showMaximized()
 
     if mode == 1: # show raw color image with shape info
       print(f"The shape of the image is {self.shape}.")
@@ -263,7 +209,9 @@ class Image:
       plt.title("color mask")
       plt.imshow(self.color_mask, cmap='gray')
       plt.subplot(2,2,4)
-      plt.title("canny edge detection with hough transformation")
+      plt.title("canny edge detection with hough transformation within ROI")
       plt.imshow(self.canny_hough_image)
 
     plt.show()
+
+    
